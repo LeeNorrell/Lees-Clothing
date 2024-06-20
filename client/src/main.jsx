8fe70939ddc1edf388856c11e1cd3098b850091f
LeeNorrell <156039272+LeeNorrell@users.noreply.github.com>
2024-06-19 20:35:03 -0500

    trying to change thi branch

---
+++ client/src/main.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import App from './App.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+import Homepage from './pages/Homepage.jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <RouterProvider router={router} />
+)
